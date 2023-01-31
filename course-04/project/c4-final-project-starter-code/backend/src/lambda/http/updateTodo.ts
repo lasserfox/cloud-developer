90ea79688cc5d7f9a6b82227c00f24c22590fbc4
jorge.fernandez <jorgeluis.fernandez.next@bbva.com>
2023-01-31 16:47:00 +0100

    added reqvalidator

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
@@ -14,6 +14,19 @@ export const handler = middy(
     const todoId = event.pathParameters.todoId
     const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
     logger.info('update1', {'todoId': todoId,'updatedTodo': updatedTodo })
+    if (!updatedTodo.name){
+    return {
+      statusCode: 404,
+      headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*' // replace with hostname of frontend (CloudFront)
+    },
+      body: JSON.stringify({
+        'msg': 'item empty'
+      })
+    }
+    }
+
     // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
     const validTodoId = await todoItemExists(event, todoId)
    logger.info('update2', {'isvalid': validTodoId })
